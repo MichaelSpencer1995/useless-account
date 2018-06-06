6c25c357f54a5827c0e8ba4908e112348af456f5
michael.spencer1995 <michael.spencer1@g.austincc.edu>
2018-06-06 17:20:58 -0500

    added folder structure

---
+++ src/components/accounts_list.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class AccountsList extends Component {
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default AccountsList;
---
+++ src/components/create_account_form.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class CreateAccountForm extends Component {
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default CreateAccountForm;
